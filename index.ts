974740889527c9b1c5b9fe6a72b38afbafa8dd50
cynthiadaubert <cynthiaperezdaubert@gmail.com>
2022-05-19 20:36:54 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,27 @@
+import * as products from "./products.json";
+
+class Product {
+  constructor(name: string, price: number) {
+    this.name = name;
+    this.price = price;
+  }
+  id: number;
+  name: string;
+  price: number;
+}
+
+class User {
+  constructor(name: string) {
+    this.name = name;
+  }
+  name: string;
+  products: Product[] = [];
+  addProduct(newProduct: Product) {
+    this.products.push(newProduct);
+  }
+  addProducts(newProducts: Product[]) {
+    this.products.push(newProducts);
+  }
+}
+
+export { User, Product };
